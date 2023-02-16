8c649d93fdf3c59a28ef5efce8e5abdbf2951d84
kr-sahilj <123371088+kr-sahilj@users.noreply.github.com>
2023-02-16 19:11:39 +0530

    final changes

--- ownToken/contracts/Faucet.sol
+++ ownToken/contracts/Faucet.sol
@@ -2,92 +2,58 @@
 
 pragma solidity 0.8.17;
 
-interface ERC20{
+interface ERC20 {
     //use to talk to other contract
-    function transfer(address to, uint256 amount) external returns(bool);
-    function balanceOf(address account) external view returns(uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
 
-    event Transfer(address indexed from,address indexed to,uint value);
+    function balanceOf(address account) external view returns (uint256);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
 }
+
 contract Faucet {
     address payable owner;
     ERC20 public token;
 
-    uint256 public withDrawlAmount; 
+    uint256 public withDrawlAmount;
     uint256 public lockTime;
     uint256 public maxlimit;
-
 
     event WithDrawl(address indexed to, uint256 indexed amount);
     event Deposit(address indexed from, uint256 indexed amount);
 
-    mapping(address => uint256) nextAccessTime;
+    mapping(address => uint256) firstAccessTime;
     mapping(address => uint256) totalToken;
 
-    constructor(address tokenAddress) payable{
+    constructor(address tokenAddress) payable {
         token = ERC20(tokenAddress);
         owner = payable(msg.sender);
-        withDrawlAmount =100 * (10**18);
-        lockTime = 5 minutes;
-        maxlimit = 500*(10**18);
-
+        lockTime = 4 minutes;
+        maxlimit = 500 * (10**18);
     }
 
-    function requestTokens() public {
-
+    function requestTokens(uint256 _withDrawlAmount) public {
+        _withDrawlAmount*=(10**18);
+        if (block.timestamp > firstAccessTime[msg.sender] + lockTime) {
+            totalToken[msg.sender] = 0;
+            firstAccessTime[msg.sender] = block.timestamp;
+        }
+        require(totalToken[msg.sender] + _withDrawlAmount <= maxlimit,"today's maximum limit reached");
         
-        if(totalToken[msg.sender]!=0)
-        {
-            //old user
-            if(totalToken[msg.sender]==maxlimit)
-            {
-                if( block.timestamp > nextAccessTime[msg.sender]+lockTime)
-                {
-                    totalToken[msg.sender]=0;
-                    nextAccessTime[msg.sender]=block.timestamp;
-                }
-                else{
-                    require(totalToken[msg.sender] > maxlimit,"today's maximum limit reached");
-                }
-                
-            }
-            // require(totalToken[msg.sender] > maxlimit,"today's maximum limit reached");
-            // if time ends but maximum token limit still left
-            if(nextAccessTime[msg.sender]+lockTime <= block.timestamp)
-            {
-                totalToken[msg.sender]=0;
-                token.transfer(msg.sender,withDrawlAmount);
-                totalToken[msg.sender]+=withDrawlAmount;
-                nextAccessTime[msg.sender]=block.timestamp;
-                
-            }
-            else
-            {
-                token.transfer(msg.sender,withDrawlAmount);
-                totalToken[msg.sender]+=withDrawlAmount;
-            }
-            
-        }
-        else{
-            //new user
-            token.transfer(msg.sender,withDrawlAmount);
-            nextAccessTime[msg.sender]=block.timestamp;
-            totalToken[msg.sender]+=withDrawlAmount;
-
-        }
+        token.transfer(msg.sender, _withDrawlAmount);
+        totalToken[msg.sender] += _withDrawlAmount;
     }
 
     // receive() external payable {
     //     emit Deposit(msg.sender, msg.value);
     // }
 
-    function getBalance() external view returns(uint256) {
+    function getBalance() external view returns (uint256) {
         return token.balanceOf(address(this));
     }
 
-
-    function setLockTime(uint256 amount) public onlyOwner{
-        lockTime = amount*1 minutes;
+    function setLockTime(uint256 amount) public onlyOwner {
+        lockTime = amount * 1 minutes;
     }
 
     function withDrawl() external onlyOwner {
@@ -99,5 +65,4 @@ contract Faucet {
         require(msg.sender == owner, "Only the contract onwer can call this");
         _;
     }
-
 }
