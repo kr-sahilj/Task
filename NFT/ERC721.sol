70c04b9c8332b92be38b93bd58053bfb2013ac1a
kr-sahilj <123371088+kr-sahilj@users.noreply.github.com>
2023-02-21 19:14:08 +0530

    Done ERC721 NFT with Old Token

---
+++ NFT/ERC721.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+// Token name
+
+
+contract ERC721 {
+    string private name;
+    string private symbol;
+
+    mapping(uint256 => address) private _owners;
+    mapping(address => uint256) private _balances;
+
+    //events
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+
+    constructor(string memory name_, string memory symbol_) {
+        name = name_;
+        symbol = symbol_;
+    }
+    
+    function _mint(address to, uint256 tokenId) internal virtual {
+        require(to != address(0), "ERC721: mint to the zero address");
+        require(!_exists(tokenId), "ERC721: token already minted");
+
+        _beforeTokenTransfer(address(0), to, tokenId, 1);
+
+        // Check that tokenId was not minted by `_beforeTokenTransfer` hook
+        require(!_exists(tokenId), "ERC721: token already minted");
+
+        unchecked {
+            // Will not overflow unless all 2**256 token ids are minted to the same owner.
+            // Given that tokens are minted one by one, it is impossible in practice that
+            // this ever happens. Might change if we allow batch minting.
+            // The ERC fails to describe this case.
+            _balances[to] += 1;
+        }
+
+        _owners[tokenId] = to;
+
+        emit Transfer(address(0), to, tokenId);
+
+        _afterTokenTransfer(address(0), to, tokenId, 1);
+    }
+
+    function _ownerOf(uint256 tokenId) internal view virtual returns (address) {
+        return _owners[tokenId];
+    }
+
+    function _exists(uint256 tokenId) internal view virtual returns (bool) {
+        return _ownerOf(tokenId) != address(0);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256, /* firstTokenId */
+        uint256 batchSize
+    ) internal virtual {
+        if (batchSize > 1) {
+            if (from != address(0)) {
+                _balances[from] -= batchSize;
+            }
+            if (to != address(0)) {
+                _balances[to] += batchSize;
+            }
+        }
+    }
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 firstTokenId,
+        uint256 batchSize
+    ) internal virtual {}
+}
---
+++ NFT/knackNFT1.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+interface ERC20 {
+    //use to talk to other contract
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);    
+}
+
+contract KnackNFT {
+    ERC20 public token;
+    address public owner;
+    address public contractAddress;
+    string public name;
+    string public symbol;
+    uint private count;
+    uint public amount;
+    mapping(uint256 => address) nftOwners;
+    mapping(address => uint256) nftBalance;
+    // Mapping from token ID to approved address
+    mapping(uint256 => address) private _tokenApprovals;
+
+    //events
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 indexed _tokenId
+    );
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+
+    constructor() {
+        name = "KnackNFT";
+        symbol = "KFT";
+        count=0;
+        owner = msg.sender;
+        amount= 100*(10**18);
+        token = ERC20(0x0fE7297c391362f6103e0a36566AF215FB87A168);
+        contractAddress=address(this);
+    }
+
+    // function _approve(address to) public {
+    //     return token.approve(to,amount);
+    // }
+
+    // function _allowance(address to) public returns(uint){
+    //     return token.allowance(owner, to);
+    // }
+    
+    function safeMint(address to) public {
+        uint256 tokenId = count;
+        count++;
+        mint(to, tokenId);
+    }
+
+    function mint(address to, uint256 tokenId) private {
+        
+        require(to != address(0), "Mint to the zero address");
+        require(!_exists(tokenId), "Token already minted");
+        // require(nftBalance[to] == 0, "You have already minted token");
+        // require()
+        nftBalance[to]+= 1;
+
+        nftOwners[tokenId] = to;
+        
+
+        token.transferFrom(to, owner, amount);
+
+        emit Transfer(address(0), to, tokenId);
+    }
+
+    function _transfer(address from, address to, uint256 tokenId) public   {
+        require(to != address(0), "transfer to the zero address");
+
+        // Check that tokenId was not transferred by `_beforeTokenTransfer` hook
+        require(_tokenApprovals[tokenId] == from || _ownerOf(tokenId) == from, "You don't have permission or You're not the owner");
+
+        // Clear approvals from the previous owner
+        delete _tokenApprovals[tokenId];
+
+        unchecked {
+            nftBalance[from] -= 1;
+            nftBalance[to] += 1;
+        }
+        nftOwners[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
+
+    }
+    function _approve(address to, uint256 tokenId) public virtual {
+        _tokenApprovals[tokenId] = to;
+        emit Approval(_ownerOf(tokenId), to, tokenId);
+    }
+
+    function _ownerOf(uint256 tokenId) public view returns (address) {
+        return nftOwners[tokenId];
+    }
+
+    function _exists(uint256 tokenId) public view virtual returns (bool) {
+        return _ownerOf(tokenId) != address(0);
+    }
+
+    
+}
